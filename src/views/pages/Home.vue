b9cdf62b6dc378c2c62d04f17bf13b96d5f72ae7
diskit <diskit.1980@gmail.com>
2020-08-01 10:59:52 +0900

    apply pug

---
+++ src/views/pages/Home.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+.home
+  img(alt="Vue logo" src="../assets/logo.png")
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Home",
+  components: {
+  }
+});
+</script>
